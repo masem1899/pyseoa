0b61c87ddda601f3243033ef71e2adb54819143a
Mario SEMPER <mario.semper@uniqa.at>
2025-05-22 21:28:39 +0200

    First commit

---
+++ pyseoa/__init__.py
@@ -0,0 +1,11 @@
+"""
+pyseoa: Python SEO Analyzer
+"""
+
+from .analyzer import SEOAnalyzer, BatchSEOAnalyzer
+
+__all__         = ['SEOAnalyzer','BatchSEOAnalyzer']
+__version__     = "0.1.0"
+__author__      = "masem"
+__email__       = "contact@masem.at"
+__url__         = ""
